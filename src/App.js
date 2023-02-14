103f79c48e642e096c4aa6468e9cb27ca82a36e1
Raison <raison024@gmail.com>
2023-02-14 14:26:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,32 +2,66 @@ import React, { useState } from "react";
 import { QrReader } from "react-qr-reader";
 import "./App.css";
 
-export default function App() {
-  const [result, setResult] = useState("No result");
-  let handleScan = data => {
-    if (data) {
-      setResult(data);
-    }
-  };
+const App = (props) => {
+  const [data, setData] = useState('No result');
 
-  let handleError = err => {
-    // alert(err);
-  };
   return (
     <div className="App-header">
-      <h1>Store</h1>
-      <div style={{height: '300px', width: '300px'}}>
-        <QrReader
-          delay={10000}
-          onError={handleError}
-          // onScan={handleScan}
-          style={{ width: "50%" }}
-          // facingMode="environment"
-          onResult={ handleScan }
-          constraints={ {facingMode: 'environment'} }
-        />
-      <p>{result}</p>
-      </div>
+    <div style={{width: '500px'}}>
+      <QrReader
+        onResult={(result, error) => {
+          if (!!result) {
+            setData(result?.text);
+          }
+
+          if (!!error) {
+            console.info(error);
+          }
+        }}
+        style={{ width: '50%' }}
+      />
+      <p>{data}</p>
+    </div>
     </div>
   );
-}
+};
+
+export default App;
+
+// export default function App() {
+//   const [result, setResult] = useState("No result");
+//   let handleScan = data => {
+//     if (data) {
+//       const joel = 'rae'
+//       // setResult(data);
+//       console.log(data);
+//       // if(data===joel) {
+//       //   console.log('this is joel');
+//       // }
+//       // else {
+//       //   console.log('this is raison')
+//       // }
+//     }
+//   };
+
+//   let handleError = err => {
+//     // alert(err);
+//   };
+//   return (
+//     <div className="App-header">
+//       <h1>Store</h1>
+//       <div style={{height: '300px', width: '300px'}}>
+//         <QrReader
+//           delay={100}
+//           onError={handleError}
+//           // onScan={handleScan}
+//           style={{ width: "50%" }}
+//           // facingMode="environment"
+//           onResult={ handleScan }
+//           constraints={ {facingMode: 'environment'} }
+//         />
+//       <p>{result}</p>
+//       </div>
+//     </div>
+//   );
+// }
