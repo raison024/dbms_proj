920cb6f9c38396fa0940d47371b3b8a7b6c1e7d8
Raison <raison024@gmail.com>
2023-02-14 14:31:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = (props) => {
   return (
     <div className="App-header">
       <h3>Virtual Cart</h3>
-    <div style={{width: '500px'}}>
+    <div style={{width: '200px'}}>
       <QrReader
         facingMode="environment"
         onResult={(result, error) => {
